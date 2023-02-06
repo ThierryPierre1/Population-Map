fded09962ecb5cc1d885035999d907077feaac15
Tpierre31 <thierry.pierre@stonybrook.edu>
2023-02-05 21:44:36 -0500

    Update Map.py

--- Map.py
+++ Map.py
@@ -8,6 +8,10 @@ from geopy.geocoders import Nominatim
 df = pd.read_csv('/Users/thierrypierre/Documents/GitHub/Population-Map/data/Population_Race.csv')
 population_byRace = df.groupby(['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming', 'Puerto Rico']).sum()
 
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', None)
+pd.set_option('display.width', None)
+
 # Create a map
 geolocator = Nominatim(user_agent="race")
 location = geolocator.geocode("United States")
